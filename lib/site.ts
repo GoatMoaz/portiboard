14cc0470d5656aed27fef6157370deb455ee21df
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2026-04-22 20:41:32 +0200

    feat: simplify site URL retrieval logic

--- lib/site.ts
+++ lib/site.ts
@@ -1,10 +1,5 @@
 export function getSiteUrl(): string {
-  const siteUrl =
-    process.env.NEXT_PUBLIC_SITE_URL ??
-    process.env.SITE_URL ??
-    (process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000");
+  const siteUrl = process.env.VERCEL_URL || "http://localhost:3000";
 
   return siteUrl.replace(/\/$/, "");
 }
